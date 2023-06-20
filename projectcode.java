ab3d4ae645db5e7adaba40d838da02c48e37dcb3
pruthuvikalhara <137063716+pruthuvikalhara@users.noreply.github.com>
2023-06-20 16:25:22 +0530

    Update projectcode.java

--- projectcode.java
+++ projectcode.java
@@ -1,15 +1,3 @@
-Jenkinsfile (Declarative Pipeline)
-/* Requires the Docker Pipeline plugin */
-pipeline {
-    agent { docker { image 'maven:3.9.0-eclipse-temurin-11' } }
-    stages {
-        stage('build') {
-            steps {
-                sh 'mvn --version'
-            }
-        }
-    }
-}
 /*
 * To change this template, choose Tools | Templates
 * and open the template in the editor.
